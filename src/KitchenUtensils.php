7a5c8891fa93ff0684416ed81e9a85b4b3af8ab1
Dmitrii Kopasov <dollars.dmitrii@yandex.ru>
2025-08-08 10:20:30 +0400

    Refactor: change class name file

--- src/KitchenUtensils.php
+++ src/KitchenUtensils.php
@@ -2,9 +2,12 @@
 
 namespace DKopasov\Library;
 
-class Library
+class KitchenUtensils
 {
 
+    /**
+     * Метод проверяет, является ли указанная строка атрибутом кухни
+    */
     public function isKitchenItem(string $item): bool
     {
 
